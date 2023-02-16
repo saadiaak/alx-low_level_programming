afef5b8ea88b4ea05585df471242255d854ee8a4
saadiaak <saadia.akharraz@gmail.com>
2023-02-16 11:42:15 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+include < stdio.h >
+
+/**
+ * main - Entry point
+ *
+ * Description: Entry point for all C programs
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
