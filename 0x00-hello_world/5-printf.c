e5dfb0003cd6bcc248d70d232917325b445d6453
saadiaak <saadia.akharraz@gmail.com>
2023-02-16 11:59:26 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'main function is used as an entry point in all c programs'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
