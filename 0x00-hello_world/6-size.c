4d65d8c7784181005942163eea5befdfd2f39ecc
saadiaak <saadia.akharraz@gmail.com>
2023-02-16 12:01:54 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * Description: 'main is the entry point for all c program'
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char charType;
+	int integerType;
+	long int longintType;
+	long long int longlongintType;
+	float floatType;
+
+	printf("Size of a char: %ld byte(s)\n", sizeof(charType));
+	printf("Size of an int: %ld byte(s)\n", sizeof(integerType));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(longintType));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(longlongintType));
+	printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
+
+   	return(0);
+}
